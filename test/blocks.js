ba4b9deae0248470f368750ba74daacba9bd6702
Altcoin Cash <altcoincash@protonmail.com>
2020-10-04 00:37:41 +0200

    lodash

--- test/blocks.js
+++ test/blocks.js
@@ -4,7 +4,7 @@ var should = require('should');
 var sinon = require('sinon');
 var BlockController = require('../lib/blocks');
 var helpthehomeless = require('@Altcoin-Cash/helpthehomeless-lib');
-var _ = require('lohelpthehomeless');
+var _ = require('lodash');
 
 var blocks = require('./data/blocks.json');
 
